ece58e84035ddf2ba88435e2c89939f37755134d
teraharas <triple.zero.option@gmail.com>
2015-11-28 21:30:38 +0000

    Correct Resize

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -56,14 +56,14 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   "#{Time.now.strftime('%Y%m%d%H%M%S%L')}.jpg"
   # end
   
-  def filename
-    "#{secure_token}.jpg" if original_filename.present?
-  end
+  # def filename
+  #   "#{secure_token}.jpg" if original_filename.present?
+  # end
   
-  protected
-  def secure_token
-    var = :"@#{mounted_as}_secure_token"
-    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
-  end
+  # protected
+  # def secure_token
+  #   var = :"@#{mounted_as}_secure_token"
+  #   model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+  # end
 
 end
