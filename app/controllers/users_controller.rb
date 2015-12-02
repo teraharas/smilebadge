c00db499a3ec13621d95aca50464d99c5c0ff11d
teraharas <triple.zero.option@gmail.com>
2015-12-02 11:07:48 +0000

    Correct Select User SQL

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def index
     # 自分以外のユーザーを取得する。
     # ソート：カナ（昇順）
-    @users = User.where('id <> ?', current_user.id).order(:email)
+    @users = User.where('id <> ?', current_user.id).order('kananame COLLATE "C"')
   end
   
   def show
