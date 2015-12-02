0ab187eb3edabcbe9abc833da5d1e028503b8ec3
teraharas <triple.zero.option@gmail.com>
2015-12-02 00:17:58 +0000

    Correct Image function

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -21,7 +21,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}" + "_1"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     # binding.pry
   end
 
