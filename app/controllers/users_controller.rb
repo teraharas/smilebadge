ae86f159249ff58b3e2f772b0e9201d3a414b0f3
teraharas <triple.zero.option@gmail.com>
2015-12-03 05:45:18 +0000

    Graph Method Refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,82 +18,61 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     
-    
-    # 30日間に獲得したバッジ
-    badgejoin_recept30days = Badge.arel_table
-    badgepostjoin_recept30days = Badgepost.arel_table
-    
-    join_condition_recept30days = badgejoin_recept30days.join(badgepostjoin_recept30days, Arel::Nodes::OuterJoin)
-              .on(badgejoin_recept30days[:id].eq(badgepostjoin_recept30days[:badge_id])).join_sources
-
-    @badgeposts_recept30days = Badge.joins(join_condition_recept30days).group(:id, :name, :image, :outputnumber)
-            .select(badgejoin_recept30days[:id], badgejoin_recept30days[:name], badgejoin_recept30days[:image], badgejoin_recept30days[:outputnumber], badgejoin_recept30days[:id].count.as('cnt'))
-            .where(badgepostjoin_recept30days[:recept_user_id].eq(current_user.id))
-            .where(badgepostjoin_recept30days[:created_at].gt 30.days.ago)
-            .order('cnt DESC')
-            # .limit(10)
-    data = Array.new
-    
-    @badgeposts_recept30days.each do |badgepost|
-      data.push([badgepost.name, badgepost.cnt])
-    end
-    
-    @graph_recept30days = LazyHighCharts::HighChart.new('graph') do |f|
-      f.title(text: '30日間に獲得したバッジバランス')
-      f.series(name: 'バッジ数', data: data, type: 'pie')
-    end
-    
-    
+    # 30日間に獲得したバッジのグラフ
+    @graph_recept30days = get_graph(@user.id, "30DAYS_RECEPT")
    
-    # 今までに獲得したバッジ
-    badgejoin_recept = Badge.arel_table
-    badgepostjoin_recept = Badgepost.arel_table
+    # 今までに獲得したバッジのグラフ
+    @graph_recept = get_graph(@user.id, "ALL_RECEPT")
     
-    join_condition_recept = badgejoin_recept.join(badgepostjoin_recept, Arel::Nodes::OuterJoin)
-              .on(badgejoin_recept[:id].eq(badgepostjoin_recept[:badge_id])).join_sources
-
-    @badgeposts_recept = Badge.joins(join_condition_recept).group(:id, :name, :image, :outputnumber)
-            .select(badgejoin_recept[:id], badgejoin_recept[:name], badgejoin_recept[:image], badgejoin_recept[:outputnumber], badgejoin_recept[:id].count.as('cnt'))
-            .where(badgepostjoin_recept[:recept_user_id].eq(current_user.id))
+    # 今までに贈ったバッジのグラフ
+    @graph_sent = get_graph(@user.id, "ALL_SENT")
+  end
+  
+  def get_graph(user_id, type)
+    # グラフインスタンス取得
+    
+    badgejoin = Badge.arel_table
+    badgepostjoin = Badgepost.arel_table
+    
+    join_condition = badgejoin.join(badgepostjoin, Arel::Nodes::OuterJoin)
+              .on(badgejoin[:id].eq(badgepostjoin[:badge_id])).join_sources
+    
+    if type == "30DAYS_RECEPT"
+      @badgeposts = Badge.joins(join_condition).group(:id, :name, :image, :outputnumber)
+            .select(badgejoin[:id], badgejoin[:name], badgejoin[:image], badgejoin[:outputnumber], badgejoin[:id].count.as('cnt'))
+            .where(badgepostjoin[:recept_user_id].eq(user_id))
+            .where(badgepostjoin[:created_at].gt 30.days.ago)
             .order('cnt DESC')
-            # .limit(10)
+    elsif type == "ALL_RECEPT"
+      @badgeposts = Badge.joins(join_condition).group(:id, :name, :image, :outputnumber)
+            .select(badgejoin[:id], badgejoin[:name], badgejoin[:image], badgejoin[:outputnumber], badgejoin[:id].count.as('cnt'))
+            .where(badgepostjoin[:recept_user_id].eq(user_id))
+            .order('cnt DESC')
+    else
+      @badgeposts = Badge.joins(join_condition).group(:id, :name, :image, :outputnumber)
+            .select(badgejoin[:id], badgejoin[:name], badgejoin[:image], badgejoin[:outputnumber], badgejoin[:id].count.as('cnt'))
+            .where(badgepostjoin[:sent_user_id].eq(user_id))
+            .order('cnt DESC')
+    end
+
     data = Array.new
     
-    @badgeposts_recept.each do |badgepost|
+    @badgeposts.each do |badgepost|
       data.push([badgepost.name, badgepost.cnt])
     end
     
-    @graph_recept = LazyHighCharts::HighChart.new('graph') do |f|
-      f.title(text: '今までに獲得したバッジバランス')
-      f.series(name: 'バッジ数', data: data, type: 'pie')
-    end
-    
-    
-    
-    # 今までに贈ったバッジ
-    badgejoin_sent = Badge.arel_table
-    badgepostjoin_sent = Badgepost.arel_table
-    
-    join_condition_sent = badgejoin_sent.join(badgepostjoin_sent, Arel::Nodes::OuterJoin)
-              .on(badgejoin_sent[:id].eq(badgepostjoin_sent[:badge_id])).join_sources
-
-    @badgeposts_sent = Badge.joins(join_condition_sent).group(:id, :name, :image, :outputnumber)
-            .select(badgejoin_sent[:id], badgejoin_sent[:name], badgejoin_sent[:image], badgejoin_sent[:outputnumber], badgejoin_sent[:id].count.as('cnt'))
-            .where(badgepostjoin_sent[:sent_user_id].eq(current_user.id))
-            .order('cnt DESC')
-            # .limit(10)
-    data = Array.new
-    
-    @badgeposts_sent.each do |badgepost|
-      data.push([badgepost.name, badgepost.cnt])
-    end
-    
-    @graph_sent = LazyHighCharts::HighChart.new('graph') do |f|
-      f.title(text: '今までに贈ったバッジバランス')
+    @graph = LazyHighCharts::HighChart.new('graph') do |f|
+      # グラフタイトル
+      if type == "30DAYS_RECEPT"
+        f.title(text: '30日間に獲得したバッジバランス')
+      elsif type == "ALL_RECEPT"
+        f.title(text: '今までに獲得したバッジバランス')
+      else
+        f.title(text: '今までに贈ったバッジバランス')
+      end
       f.series(name: 'バッジ数', data: data, type: 'pie')
     end
   end
-  
   
   def new
     @user = User.new
