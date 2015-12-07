b46e04d9d9aceae1c222c6908e33dd026b28a387
teraharas <triple.zero.option@gmail.com>
2015-12-07 05:18:16 +0000

    Correct Mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,6 +20,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
         address: ENV["SMTP_ADDRESS"],
         port: ENV["SMTP_PORT"],
+        domain: ENV["SMTP_DOMAIN"],
         user_name: ENV["SMTP_USER_NAME"],
         password: ENV["SMTP_PASSWORD"]
   }
