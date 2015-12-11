46b8491ccd09263b287168abffbd190a42e6be0b
teraharas <triple.zero.option@gmail.com>
2015-12-11 06:50:05 +0000

    Patch

--- config/routes.rb
+++ config/routes.rb
@@ -10,13 +10,6 @@ Rails.application.routes.draw do
   get 'badgeposts/recepting'
   get 'badgeposts/sending'
   get 'users/full_index'
-  
-  get '/reset_password', to: "reset_password#new"
-  resources :reset_password, only: [:create]
-  get "/update_password/:reset_password_token", to: "update_password#new", as: "update_password"
-  resources :update_password, only: [:create]
-  get "/invalid_token", to: "invalid_token#index"
-	get "/reset_password_confirmation", to: "reset_password_confirmation#index"
 
   resources :users
   resources :bumons
