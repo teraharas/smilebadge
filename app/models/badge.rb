37b45c771e559eb3f6b6e4476372c55d2fb80421
teraharas <triple.zero.option@gmail.com>
2015-12-14 19:50:17 +0000

    Add Validation

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,10 +1,12 @@
 class Badge < ActiveRecord::Base
-    # 画像アップロード
-    mount_uploader :image, ImageUploader
-    
+    validates :name, presence: true, length: { maximum: 50 }
+    validates :explanation, presence: true, length: { maximum: 255 }
     validates :outputnumber, presence: true,
                         numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to:10 },
                         uniqueness: { case_sensitive: false }
+                        
+    # 画像アップロード
+    mount_uploader :image, ImageUploader
                         
     has_many :badgeposts, :class_name => "Badgepost", :foreign_key => "badge_id"
 end
