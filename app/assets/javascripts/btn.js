1d8ca2842f7c5225589a129e60e689beb80453aa
teraharas <triple.zero.option@gmail.com>
2015-12-17 13:29:43 +0000

    Patch

--- app/assets/javascripts/btn.js
+++ app/assets/javascripts/btn.js
@@ -1,8 +1,8 @@
-function set_badge_id(badge_id) {
+function set_badge_id(badge_id, usablebadgecount) {
     // バッジ送付可能数0以下ならチェックONにしない
-    // if (usablebadgecount <= 0) {
-    //     exit;
-    // }
+    if (usablebadgecount <= 0) {
+        exit;
+    }
     // バッジIDを隠し項目に退避する
     $(':hidden[name="badgepost[badge_id]"]').val(badge_id);
     // チェックマークの表示非表示
