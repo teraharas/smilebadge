1cc051a79fd6b03d58db47f08fe54ca52ac8f575
teraharas <triple.zero.option@gmail.com>
2015-12-18 08:49:08 +0000

    Patch

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { :host => 'yourapp.heroku.com' }
+  config.action_mailer.default_url_options = { :host => ENV["APPLI_URL"] }
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
