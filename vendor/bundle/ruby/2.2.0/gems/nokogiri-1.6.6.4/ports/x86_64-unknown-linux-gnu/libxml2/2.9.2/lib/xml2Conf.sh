37e8c6187936526dd23dff959ae02e0b7d2892e7
teraharas <triple.zero.option@gmail.com>
2015-12-19 13:38:57 +0000

    Delete vendor

--- vendor/bundle/ruby/2.2.0/gems/nokogiri-1.6.6.4/ports/x86_64-unknown-linux-gnu/libxml2/2.9.2/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/home/ubuntu/workspace/smilebadge/vendor/bundle/ruby/2.2.0/gems/nokogiri-1.6.6.4/ports/x86_64-unknown-linux-gnu/libxml2/2.9.2/lib"
-XML2_LIBS="-lxml2 -lz   -lm "
-XML2_INCLUDEDIR="-I/home/ubuntu/workspace/smilebadge/vendor/bundle/ruby/2.2.0/gems/nokogiri-1.6.6.4/ports/x86_64-unknown-linux-gnu/libxml2/2.9.2/include/libxml2"
-MODULE_VERSION="xml2-2.9.2"
-
