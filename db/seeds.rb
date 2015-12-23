af3e5e9a6c3b6c57a5ee40a09b46e53fe17cbc2e
teraharas <triple.zero.option@gmail.com>
2015-12-23 02:53:09 +0000

    Correct seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,9 +20,9 @@
 #               bumon_id: bumon_id)
 # end
 
-users = User.order(:created_at).take(6)
-20000.times do
-  content = Faker::Lorem.sentence(5)
-  users.each { |user| user.sent_badgeposts.create!(sent_user_id: user.id,
-                          recept_user_id: user.id + 1, badge_id: user.id + 1, content: content) }
-end
+# users = User.order(:created_at).take(6)
+# 20000.times do
+#   content = Faker::Lorem.sentence(5)
+#   users.each { |user| user.sent_badgeposts.create!(sent_user_id: user.id,
+#                           recept_user_id: user.id + 1, badge_id: user.id + 1, content: content) }
+# end
