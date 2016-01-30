ccc26988d6da9ef213f4f729ae7224e5a94053d2
teraharas <triple.zero.option@gmail.com>
2016-01-30 12:23:24 +0000

    From SCSS TO SASS

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Smilebadge
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :ja
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
@@ -28,5 +28,7 @@ module Smilebadge
     config.assets.initialize_on_precompile = false
     
     config.active_job.queue_adapter = :delayed_job # アダプターの設定を追記
+    
+    config.sass.preferred_syntax = :sass
   end
 end
