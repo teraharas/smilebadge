eec0af22c616815ec0a5b653f564b5a82c93f982
teraharas <triple.zero.option@gmail.com>
2016-02-02 09:26:58 +0000

    Remove Badgepost Limit

--- app/assets/javascripts/btn.js
+++ app/assets/javascripts/btn.js
@@ -1,13 +1,9 @@
-function set_badge_id(badge_id, badge_name, usablebadgecount) {
-    // バッジ送付可能数0以下ならチェックONにしない
-    if (usablebadgecount <= 0) {
-        exit;
-    }
-    // バッジIDを隠し項目に退避する
-    $(':hidden[name="badgepost[badge_id]"]').val(badge_id);
-    // チェックマークの表示非表示
-    $(".badge_checked").hide();
-    $("#badge_checked_" + badge_id).show();
-    
-    document.getElementById("selected_badge_name").innerText = badge_name + "バッジを選択中！";
+function set_badge_id(badge_id, badge_name) {
+  // バッジIDを隠し項目に退避する
+  $(':hidden[name="badgepost[badge_id]"]').val(badge_id);
+  // チェックマークの表示非表示
+  $(".badge_checked").hide();
+  $("#badge_checked_" + badge_id).show();
+  
+  document.getElementById("selected_badge_name").innerText = badge_name + "バッジを選択中！";
 }
