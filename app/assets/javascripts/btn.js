3a6ba7d8f71804479b000d7baeccece2aaaf0cc6
teraharas <triple.zero.option@gmail.com>
2016-02-12 14:06:27 +0000

    Add Badge Quick Spin Action

--- app/assets/javascripts/btn.js
+++ app/assets/javascripts/btn.js
@@ -5,5 +5,15 @@ function set_badge_id(badge_id, badge_name) {
   $(".badge_checked").hide();
   $("#badge_checked_" + badge_id).show();
   
+  // バッジの小刻みな回転アクションの追加
+  $(".largebadgeimage").css('animation', '')
+  $("#id_badgelargeimage_" + badge_id).css('animation', 'shake 0.5s infinite linear')
+  $("#id_badgelargeimage_" + badge_id).css('animation-iteration-count', 'infinite')
+  $("#id_badgelargeimage_" + badge_id).css('animation-duration', '10s')
+  $(".largebadgeimage").css('-webkit-animation', '')
+  $("#id_badgelargeimage_" + badge_id).css('-webkit-animation', 'shake 0.5s infinite linear')
+  $("#id_badgelargeimage_" + badge_id).css('-webkit-animation-iteration-count', 'infinite')
+  $("#id_badgelargeimage_" + badge_id).css('-webkit-animation-duration', '10s')
+
   document.getElementById("selected_badge_name").innerText = badge_name + "バッジを選択中！";
 }
