c671e28e0cb0563d02dbe9e00649f18307538676
teraharas <triple.zero.option@gmail.com>
2016-03-06 14:39:01 +0000

    Fix Top page img

--- config/application.rb
+++ config/application.rb
@@ -26,8 +26,6 @@ module Smilebadge
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     
-    config.assets.initialize_on_precompile = false
-    
     config.active_job.queue_adapter = :delayed_job # アダプターの設定を追記
     
     config.sass.preferred_syntax = :sass
